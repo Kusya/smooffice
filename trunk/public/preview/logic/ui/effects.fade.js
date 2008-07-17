7d0b91d559c8ef0d56008ffad974842821e34916
lrbabe <lrbabe@67f4de9f-8d4f-0410-b519-4bf5a57edfd6>
2008-07-17 16:05:58 +0000

    should fix slider problem

--- trunk/public/preview/logic/ui/effects.fade.js
+++ trunk/public/preview/logic/ui/effects.fade.js
@@ -1,13 +1,13 @@
 ;(function($) {
   
   $.effects.fade = function(o) {
-
-    return this.queue(function() {
+  	
+	return this.queue(function() {
       
       // Create element
       var el = $(this), props = ['opacity'];
-      
-      // Set options
+	  
+	  // Set options
       var mode = $.effects.setMode(el, o.options.mode || 'effect'); // Set Mode
       if (mode == 'toggle') mode = el.is(':hidden') ? 'show' : 'hide'; // Set for toggle
       var opacity = o.options.opacity || 0; // Default fade opacity
@@ -21,9 +21,8 @@
       var animation = {opacity: mode == 'show' ? original_opacity : opacity};
       
       // Animate
-      el.animate(animation, { queue: o.options.queue || false, duration: o.duration, easing: o.options.easing, complete: function() {
-        if(mode == 'hide') el.hide(); // Hide
-        if(mode == 'hide') $.effects.restore(el, props); // Restore
+      el.animate(animation, { queue: false, duration: o.duration, easing: o.options.easing, complete: function() {
+        if (mode == 'hide') { el.hide(); $.effects.restore(el, props); } // Hide & Restore
         if(o.callback) o.callback.apply(this, arguments); // Callback
         el.dequeue();
       }});
